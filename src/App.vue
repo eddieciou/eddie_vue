33c5ded19e096dbfa9e95caf8335eba7ffa8f810
eddieciou <a589658gur@gmail.com>
2023-02-21 16:15:43 +0800

    select 範例

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,49 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="p-2 flex flex-col gap-2">
+    <p>
+      <select
+        @change="changeValue($event, 'type')"
+        v-model="framework.type"
+        class="outline-none p-2 w-36 text-center border border-blue-500 rounded-lg"
+      >
+        <option value="Frontend">Frontend</option>
+        <option value="Backend">Backend</option>
+      </select>
+    </p>
+    <p>
+      <select
+        @change="changeValue($event, 'name')"
+        v-model="framework.name"
+        class="outline-none p-2 w-36 text-center border border-blue-500 rounded-lg"
+      >
+        <option value="Vue">Vue</option>
+        <option value="React">React</option>
+        <option value="Angular">Angular</option>
+        <option value="Laravel">Laravel</option>
+        <option value="CakePHP">CakePHP</option>
+        <option value="Django">Django</option>
+      </select>
+    </p>
+    <p>Type: {{ framework.type }}</p>
+    <p>Framework: {{ framework.name }}</p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+import { reactive } from "vue";
+export default {
+  name: "app",
+  setup() {
+    const framework = reactive({
+      type: "Frontend",
+      name: "Vue",
+    });
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    const changeValue = (e: Event, id: "type" | "name") => {
+      framework[id] = (e.target as HTMLSelectElement).value;
+    };
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+    return { framework, changeValue };
+  },
+};
+</script>
